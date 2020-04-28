69db2e4f6c17b54dff66fe6e7f0b2d4c351716ae
Mehul Shah <mehul.shah@wovvtech.com>
2020-04-28 10:55:11 +0530

    Initial Commit

---
+++ NeoSTORE_Mehul/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeoSTORE_Mehul
+//
+//  Created by webwerks on 28/04/20.
+//  Copyright © 2020 webwerks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NeoSTORE_MehulTests/NeoSTORE_MehulTests.swift
@@ -0,0 +1,34 @@
+//
+//  NeoSTORE_MehulTests.swift
+//  NeoSTORE_MehulTests
+//
+//  Created by webwerks on 28/04/20.
+//  Copyright © 2020 webwerks. All rights reserved.
+//
+
+import XCTest
+@testable import NeoSTORE_Mehul
+
+class NeoSTORE_MehulTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
